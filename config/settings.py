6739fbad977b8bb5a7142dff631d5b5b6dffc535
kravetst <taraskravetc28@gmail.com>
2025-08-18 20:20:05 +0300

    update settings and requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z_v4ltj1!dj=l8c11*sx0le7$^$a4n84x@$ww&7z(pm58g^vep'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-z_v4ltj1!dj=l8c11*sx0le7$^$a4n84x@$ww&7z(pm58g^vep')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -119,6 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+LOGIN_REDIRECT_URL = '/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
