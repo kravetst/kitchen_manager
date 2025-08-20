b7a8f92bb5def3aeb6422a427fac7d99cf2532c9
kravetst <taraskravetc28@gmail.com>
2025-08-20 15:06:55 +0300

    change allowed_hosts in settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 # Allowed hosts
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 # Database
 DATABASES = {
